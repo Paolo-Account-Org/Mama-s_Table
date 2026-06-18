c19754ca64ad057478e53608147970cbe3c5caf4
Paolo Adame <adamepaolo@gmail.com>
2026-06-17 21:02:02 -0400

    color update

--- Mamas_Table/src/views/Review.vue
+++ Mamas_Table/src/views/Review.vue
@@ -8,3 +8,15 @@
     <p>This is where you can review your previous orders and provide feedback.</p>
   </div>
 </template>
+
+<style>
+
+.review-wrapper {
+  padding: 2rem;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+
+
+</style>
